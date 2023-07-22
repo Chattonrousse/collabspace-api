5b4ca6cf85445c4817a525948e1f342207638218
Geovanna Candido da Silva <gecandido13@gmail.com>
2023-07-21 21:41:50 -0300

    Added initialized friends module and CreateFriends

---
+++ src/enums/friendsActions.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+
+export enum EnumFriendsActions {
+  requested = 1,
+  canceled = 2,
+  accepted = 3,
+  refused = 4,
+}
---
+++ src/enums/reactionTypes.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+export enum EnumReactionTypes {
+  liked = 1,
+  loved = 2,
+  laughs = 3,
+  angry = 4,
+  sad = 5,
+  notKidding = 6,
+}
